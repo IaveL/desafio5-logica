190dc80d69a4d4a9d322d284d1c712bf2905457a
Igor Avelino <igoravel21@gmail.com>
2022-01-31 15:29:21 -0300

    uploading challenge

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let animes = [`Attack on titan`, `Berserk`,`One Punch Man`,`Fullmetal`,`Death Note`];
+
+console.log(animes)
+
+animes.unshift(`Demon slayer`);
+
+console.log(animes)
+
+animes.pop()
+
+console.log(animes)
+
+animes.push(`Naruto`,`Tokyo Ghoul`)
+
+console.log(animes)
+
+animes.shift()
+
+console.log(animes);
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
